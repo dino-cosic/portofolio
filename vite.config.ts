2cc123c9e149afe2e5cd3337a0d2f83758048841
dino.cosic95 <dino.cosic95@gmail.com>
2025-11-28 17:48:53 +0100

    chore: update vite config to match new repo name and base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portofolio/', // GitHub Pages base path
+  base: '/', // GitHub Pages root URL (for username.github.io repo)
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
